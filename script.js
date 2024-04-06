2ad7a12120a04c4caaf43000c38e975d8a13e294
ZEDAFZ <138895344+ZEDAFZ@users.noreply.github.com>
2024-04-06 17:52:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript (jQuery)
+$(document).ready(function() {
+    // Toggle mobile menu
+    $('#mobileMenuBtn').click(function() {
+        $('#navLinks').toggleClass('hidden');
+    });
+
+    // Add navigation links dynamically
+    // Example:
+    // $('#navLinks').append('<a href="#" class="hover:text-gray-300">Link</a>');
+});
